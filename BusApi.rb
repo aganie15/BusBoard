5a12a20e45654a1b8070276d83c08d60f8dc3c8c
aganie15 <aga.niewiadomska@softwire.com>
2022-09-08 16:58:08 +0100

    Part 2 implementation

--- BusApi.rb
+++ BusApi.rb
@@ -1,7 +1,31 @@
+require 'httparty'
+
+def get_response(request)
+  response = HTTParty.get(request)
+  return JSON.parse(response.to_s)
+end
 class BusApi
+  def get_nearest_stop_codes(postcode)
+    postcode_info = get_response("https://api.postcodes.io/postcodes/#{postcode}")
+    longitude = postcode_info['result']['longitude']
+    latitude = postcode_info['result']['latitude']
+    nearest_stop_array = get_response("https://api.tfl.gov.uk/StopPoint/?lat=#{latitude}&lon=#{longitude}&stopTypes=NaptanPublicBusCoachTram")['stopPoints']
+    # extract useful info??
+    nearest_stop_array.sort_by! {|hsh| hsh['distance']}
+    #if nearest_stop_array.length > 2
+    #  nearest_stop_array = nearest_stop_array[0..2]
+    #end
+    stop_code_array = []
+    #puts nearest_stop_array
+    for entry in nearest_stop_array
+      stop_code_array.push(entry['naptanId'])
+    end
+    return stop_code_array
+  end
+
   def get_arrival_times(stop_code)
-    response = HTTParty.get("https://api.tfl.gov.uk/StopPoint/#{stop_code}/Arrivals")
-    bus_info = JSON.parse(response.to_s)
+    # this call is not working!
+    bus_info = get_response("https://api.tfl.gov.uk/StopPoint/#{stop_code}/Arrivals")
 
     # parse the information into a hash, which uses the vehicle_id as a key
     bus_array = []
@@ -16,7 +40,21 @@ class BusApi
     bus_array.sort_by! {|hsh| hsh['mins_to_arrival']}
 
     # select only the first 5 elements
-    bus_array = bus_array[0..5]
-    puts(bus_array)
+    if bus_array.length > 5
+      bus_array = bus_array[0..5]
+    end
+
+    # Print some bus stop info here
+    if bus_info != []
+      puts "Bus stop name: #{bus_info[1]['stationName']}"
+      puts 'Upcoming arrivals:'
+      for bus in bus_array
+        puts "Line number: #{bus['line_number']}, Destination: #{bus['destination']}, Minutes until arrival: #{bus['mins_to_arrival']}"
+      end
+      puts ''
+      return true # could use these to count the number of output arrivals in BusBoard
+    else
+      return false
+    end
   end
 end
--- bus_board.rb
+++ bus_board.rb
@@ -2,12 +2,23 @@ require 'httparty'
 require 'json'
 require './BusApi'
 
-# london bus stop code = 490008660N
+# london bus stop code = 490008660N / postcode = NW51TL
 def bus_board
-  puts 'Please enter a bus stop code'
-  stop_code = gets.chomp
+  puts 'Please enter a postcode'
+  postcode = gets.chomp
   api = BusApi.new
-  api.get_arrival_times(stop_code)
+  stop_codes = api.get_nearest_stop_codes(postcode)
+  counter = 0
+  for stop_code in stop_codes
+    if counter < 2
+      completed_sucessfully = api.get_arrival_times(stop_code)
+      if completed_sucessfully
+        counter = counter + 1
+      end
+    else
+      break
+    end
+  end
 end
 
 bus_board
